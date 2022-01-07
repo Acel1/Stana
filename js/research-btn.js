4b6de4674306dfee182a7f80b246c09b64f0313c
Axel <axel.caron@gmail.com>
2022-01-07 16:31:54 +0100

    sass

--- js/research-btn.js
+++ js/research-btn.js
@@ -1,1 +1,9 @@
-document.getElementById("research-form__btn").addEventListener("click", function () {});
+document.getElementById("research-form__btn").addEventListener("click", function () {
+  document.getElementById("main-nav").style.display = "none";
+  document.getElementById("research-form").style.display = "block";
+});
+
+document.getElementById("esc-nav-form").addEventListener("click", function () {
+  document.getElementById("research-form").style.display = "none";
+  document.getElementById("main-nav").style.display = "block";
+});
