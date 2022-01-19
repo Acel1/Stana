53d67d5505a5c841d6c6a05c36593c7ad29e46b0
Axel <axel.caron@gmail.com>
2022-01-19 16:37:31 +0100

    aa

--- js/research-btn.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("research-form-btn").addEventListener("click", function () {
-  document.getElementById("main-nav").style.display = "none";
-  document.getElementById("research-form").style.display = "block";
-});
-
-document.getElementById("esc-nav-form").addEventListener("click", function () {
-  document.getElementById("research-form").style.display = "none";
-  document.getElementById("main-nav").style.display = "block";
-});
