8fe4540f294c3a0286a056250c8771d439ff4490
Acel1 <axel.caron@gmail.com>
2022-03-09 17:29:47 +0100

    classes and scss files optimisation

--- js/script.js
+++ js/script.js
@@ -20,3 +20,24 @@ formEscBtn.addEventListener("click", function () {
   form.style.display = "none";
   leftNav.style.display = "flex";
 });
+
+//DROPDOWNS
+
+let dropdown = document.querySelectorAll(".dropdown");
+let dropdownBtn = document.querySelectorAll(".dropdown-btn");
+let subMenu = document.querySelectorAll(".dropdown ul");
+let dropdownLi = document.querySelectorAll(".dropdown li");
+
+let toggleIndex;
+
+dropdownBtn.addEventListener("click", toggleDropdown);
+
+function toggleDropdown() {
+  if (!toggleIndex) {
+    subMenu.style.height = `${subMenu.scrollHeight}px`;
+    toggleIndex = true;
+    return;
+  }
+  subMenu.style.height = 0;
+  toggleIndex = false;
+}
